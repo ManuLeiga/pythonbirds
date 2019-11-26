96336a1ec7956af87f383d9e50cf9b511fa5a66b
ManuLeiga <annyemanuely200@gmail.com>
2019-11-26 16:16:10 -0300

    atributo complexo filhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,15 +1,20 @@
 class Pessoa:
-    def __init__(self, nome = None):
+    def __init__(self, *filhos, nome = None, idade = 19):
+        self.idade = idade
         self.nome = nome
+        self.filhos = list(filhos)
 
     def  cumprimentar(self):
         return f'Ola {id(self)}'
 
 if __name__ == '__main__':
-    p = Pessoa('A')
-    print(Pessoa.cumprimentar(p))
-    print(id(p))
-    print(p.cumprimentar())
-    print(p.nome)
-    p.nome = 'Anny'
-    print(p.nome)
+    renzo = Pessoa(nome = 'Renzo')
+    anny = Pessoa(renzo, nome = 'Anny')
+    print(Pessoa.cumprimentar(anny))
+    print(id(anny))
+    print(anny.cumprimentar())
+    print(anny.nome)
+    print(anny.idade)
+    for filho in anny.filhos:
+        print(filho.nome)
+    print(anny.filhos)
