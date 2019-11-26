a26b499fe6b454d8bb01fa414f3a69a3cdea5758
ManuLeiga <annyemanuely200@gmail.com>
2019-11-26 18:16:05 -0300

    decoreixons ??

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,4 +1,6 @@
 class Pessoa:
+    olhos = 2
+
     def __init__(self, *filhos, nome = None, idade = 19):
         self.idade = idade
         self.nome = nome
@@ -6,6 +8,14 @@ class Pessoa:
 
     def  cumprimentar(self):
         return f'Ola {id(self)}'
+
+    @staticmethod
+    def metodo_estatico():
+        return 42
+
+    @classmethod
+    def nome_e_atributos_de_classe(cls):
+        return  f'{cls} - olhos {cls.olhos}'
 
 if __name__ == '__main__':
     renzo = Pessoa(nome = 'Renzo')
@@ -21,4 +31,10 @@ if __name__ == '__main__':
     del anny.filhos #remove atributos
     print(anny.__dict__)
     print(renzo.__dict__)
+    print(anny.olhos)
+    print(renzo.olhos)
+    print(Pessoa.olhos)
+    print(id(Pessoa.olhos), id(anny.olhos), id(renzo.olhos))
+    print(Pessoa.metodo_estatico(), anny.metodo_estatico())
+    print(Pessoa.nome_e_atributos_de_classe(), anny.nome_e_atributos_de_classe())
 
